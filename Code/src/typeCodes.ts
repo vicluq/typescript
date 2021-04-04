56faa04aa6d9d67460106af92f0f6336cfbe3e98
vicluq <victoria.tewari@gmail.com>
2021-04-04 12:25:38 -0300

    done some testing

---
+++ Code/src/typeCodes.ts
@@ -0,0 +1,43 @@
+type Gender = "man" | "woman" | "non-binary";
+
+type Cars = { brand: string, model: string }[]
+
+type Address = {
+  street: string;
+  num: number;
+  city: string;
+  country: string;
+  zipCode: number;
+};
+
+type Person = {
+  name: string;
+  age: number;
+  gender: Gender;
+  cars: Cars,
+  address: Address;
+};
+
+type IncreaseAgeFunction = (p: Person) => Person;
+
+const human: Person = {
+    name: 'Victoria Luquet',
+    gender: 'woman',
+    cars: [],
+    age: 19,
+    address: {
+        city: 'Recife',
+        country: 'Brazil',
+        num: 203,
+        street: 'Rua Estrela',
+        zipCode: 52060282,
+    }
+}
+
+const increaseAge: IncreaseAgeFunction = (person) => {
+    person.age += 1;
+    return person;
+}
+
+console.log(human);
+console.log(increaseAge(human));
